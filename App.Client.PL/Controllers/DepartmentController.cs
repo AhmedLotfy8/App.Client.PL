dbf5ad4daec6062b08377d32d16c4be242d58e68
AhmedLotfy8 <ahmedfert45@gmail.com>
2025-09-24 15:59:30 +0200

    Created "Create (Action-view)"

--- App.Client.PL/Controllers/DepartmentController.cs
+++ App.Client.PL/Controllers/DepartmentController.cs
@@ -1,4 +1,6 @@
 ﻿using App.Client.BLL.Interfaces;
+using App.Client.DAL.Models;
+using App.Client.PL.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace App.Client.PL.Controllers {
@@ -15,6 +17,33 @@ namespace App.Client.PL.Controllers {
         public IActionResult Index() {
             var departments = _departmentReposoitory.GetAll();
             return View(departments);
+        }
+
+
+        [HttpGet]
+        public IActionResult Create() {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateDepartmentDto model) {
+
+            if (ModelState.IsValid) {
+                var department = new Department() {
+                    Code = model.Code,
+                    Name = model.Name,
+                    CreateAt = model.CreateAt
+                };
+
+                var count = _departmentReposoitory.Add(department);
+
+                if (count > 0) {
+                    return RedirectToAction(nameof(Index));
+                }
+
+            }
+
+            return View(model);
         }
 
 
