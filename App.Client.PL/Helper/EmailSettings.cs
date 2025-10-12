d94f607983c7f3e45c0620285e88f5f4f1489338
AhmedLotfy8 <ahmedfert45@gmail.com>
2025-10-12 22:58:02 +0200

    send email implementation

--- App.Client.PL/Helper/EmailSettings.cs
+++ App.Client.PL/Helper/EmailSettings.cs
@@ -1,14 +1,27 @@
-﻿namespace App.Client.PL.Helper {
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace App.Client.PL.Helper {
     public class EmailSettings {
 
         public static bool SendEmail(Email email) {
-            return true;    
+
+
+            try {
+                var client = new SmtpClient("smtp.gmail.com", 587);
+                client.EnableSsl = true;
+                client.Credentials = new NetworkCredential("soradoxd@gmail.com", "oasbjkpehnupdjyp");
+                client.Send("soradoxd@gmail.com", email.To, email.Subject, email.Body);
+
+                return true;
+
+            }
+
+            catch (Exception e) {
+                return false;
+            }
+
         }
-            
-
-
-
-
 
     }
 }
